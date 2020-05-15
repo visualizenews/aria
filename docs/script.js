6bdf4b09cb5ece1ba0d216b8c05fbd6a39ef734f
Simone Lippolis <213991+leeppolis@users.noreply.github.com>
2020-05-15 21:48:00 +0200

    Maps and charts placeholders

--- docs/script.js
+++ docs/script.js
@@ -13,69 +13,128 @@ const STATIONS = [
 const SUBS = {
   PM10: {
     name: 'Polveri Sottili',
-    abbr: 'PM10',
+    code: 'PM10',
     safe: 10,
     warning: 25,
   },
   PM25: {
     name: 'Polveri Super Sottili',
-    abbr: 'PM25'
+    code: 'PM25'
   },
   NO2: {
     name: 'Diossido di azoto',
-    abbr: 'NO2'
+    code: 'NO2'
   },
   CO_8H: {
     name: 'Anidride Carbonica',
-    abbr: 'CO 8h'
+    code: 'CO 8h'
   },
   O3: {
     name: 'Ozono',
-    abbr: 'O3'
+    code: 'O3'
   },
   SO2: {
     name: 'Anidride Solforosa',
-    abbr: 'SO2'
+    code: 'SO2'
   },
   C6H6: {
     name: 'Benzene',
-    abbr: 'C6H6'
+    code: 'C6H6'
   },
 };
 
 (() => {
   const $mapContainer = document.querySelector('#page-maps');
+  const $chartContainer = document.querySelector('#page-charts');
   let rawData = [];
   let substances = [];
+  const maps = [];
+  const charts = [];
+
+  const drawCharts = () => {
+    let html = '';
+    charts.forEach((c, i) => {
+      html += `<div class="page-chart page-chart-${c.id}" data-station="${c.id}" id="page-chart-${c.id}">`;
+      html += `<div class="page-chart-title"><h2>${c.title}</h2></div>`
+      html += '<div class="page-chart-scroller">';
+      c.charts.forEach((ch, j) => {
+        html += '<div class="page-chart-wrapper">CHART</div>'
+      });
+      html += '</div>'
+      html += '</div>';
+    })
+    $chartContainer.innerHTML = html;
+  };
 
   const drawMaps = () => {
     let html = '';
-    const keys = Object.keys(SUBS);
-    keys.forEach((key, i) => {
-      if (substances.indexOf(key) > -1) {
-        html += `<div class="page-map page-map-${i}" data-sub="${key}" id="page-map-${key}">`;
-        STATIONS.forEach((s, j) => {
-          html += `<div class="marker marker-${j}" id="marker-${i}-${j}"></div>`;
-        });
-        html += `<div class="page-map-indicator">${SUBS[key].name}</div>`;
-        html += '</div>';
-      }
+    console.log(maps);
+    maps.forEach(m => {
+      html += `<div class="page-map page-map-${m.index}" data-sub="${m.id}" id="page-map-${m.id}">`;
+      html += `<div class="page-map-title"><h2>${m.label}</h2></div>`;
+      html += '<div class="page-map-container">';
+      m.markers.forEach((mk, j) => {
+          html += `<div class="marker marker-${mk.id}" id="marker-${m.index}-${mk.index}" style="background: ${mk.style.background}"></div>`;
+      });
+      html += '</div>';
+      html += '</div>';
     });
     $mapContainer.innerHTML = html;
-  }
+  };
 
   const prepareData = data => {
     console.log(data);
     rawData = data.records;
-
+    // Substances
     rawData.forEach(d => {
       if (substances.indexOf(d.inquinante.toUpperCase()) === -1) { substances.push(d.inquinante.toUpperCase()) }
     });
-
-    console.log(substances);
-
-    drawMaps();
-  }
+    // Maps
+    const keys = Object.keys(SUBS);
+    keys.forEach((key, i) => {
+      if (substances.indexOf(key) > -1) {
+        maps.push({
+          id: key,
+          index: i,
+          label: SUBS[key].name,
+          markers: (() => {
+            const markers = [];
+            STATIONS.forEach((s, j) => {
+              markers.push({
+                id: j,
+                index: j,
+                // Logic here
+                style: {
+                  background: 'var(--scale2-color)'
+                }
+              });
+            });
+            return markers;
+          })(),
+        });
+      }
+    });
+    // Charts
+    STATIONS.forEach((s, i) => {
+      charts.push({
+        id: i,
+        title: s,
+        charts: (() => {
+          const charts = [];
+          keys.forEach((key, j) => {
+            // Logic here
+            charts.push({
+              id: j,
+              title: SUBS[key].name,
+              code: SUBS[key].code,
+              data: []
+            });
+          });
+          return charts;
+        })(),
+      });
+    });
+  };
   
   const main = () => {
     fetch(
@@ -84,7 +143,12 @@ const SUBS = {
         mode: 'no-cors'
       })
       .then(response => response.json())
-      .then(data => prepareData(data));
+      .then(data => {
+        prepareData(data);
+        console.log(maps);
+        drawMaps();
+        drawCharts();
+      });
   }
 
   const ready = () => {
