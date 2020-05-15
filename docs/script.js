d7a81d74c2c2580ba7f78500c445d0e68b9581a5
Simone Lippolis <213991+leeppolis@users.noreply.github.com>
2020-05-15 14:55:16 +0200

    Dynamic maps

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-const URL = 'http://dati.comune.milano.it/api/3/action/datastore_search?resource_id=88c1e729-420e-433f-9397-875b54aa471d&limit=380';
+const URL = './data.json';
 const STATIONS = [
   'Viale Liguria',
   'Viale Marche',
@@ -10,17 +10,81 @@ const STATIONS = [
   'P.le Zavattari',
   'Parco Lambro'
 ];
+const SUBS = {
+  PM10: {
+    name: 'Polveri Sottili',
+    abbr: 'PM10',
+    safe: 10,
+    warning: 25,
+  },
+  PM25: {
+    name: 'Polveri Super Sottili',
+    abbr: 'PM25'
+  },
+  NO2: {
+    name: 'Diossido di azoto',
+    abbr: 'NO2'
+  },
+  CO_8H: {
+    name: 'Anidride Carbonica',
+    abbr: 'CO 8h'
+  },
+  O3: {
+    name: 'Ozono',
+    abbr: 'O3'
+  },
+  C6H6: {
+    name: 'Benzene',
+    abbr: 'C6H6'
+  },
+  SO2: {
+    name: 'Anidride Solforosa',
+    abbr: 'SO2'
+  },
+};
 
 (() => {
+  const $mapContainer = document.querySelector('#page-maps');
+  let rawData = [];
+  let substances = [];
+
+  const drawMaps = () => {
+    let html = '';
+    const keys = Object.keys(SUBS);
+    keys.forEach((key, i) => {
+      if (substances.indexOf(key) > -1) {
+        html += `<div class="page-map page-map-${i}" data-sub="${key}" id="page-map-${key}">`;
+        STATIONS.forEach((s, j) => {
+          html += `<div class="marker marker-${j}" id="marker-${i}-${j}"></div>`;
+        });
+        html += `<div class="page-map-indicator">${SUBS[key].name}</div>`;
+        html += '</div>';
+      }
+    });
+    $mapContainer.innerHTML = html;
+  }
+
+  const prepareData = data => {
+    console.log(data);
+    rawData = data.records;
+
+    rawData.forEach(d => {
+      if (substances.indexOf(d.inquinante.toUpperCase()) === -1) { substances.push(d.inquinante.toUpperCase()) }
+    });
+
+    console.log(substances);
+
+    drawMaps();
+  }
+  
   const main = () => {
-    // alert();
     fetch(
       URL,
       {
         mode: 'no-cors'
       })
-      .then(response => { console.log(response, response.body); response.json() })
-      .then(data => console.log(data));
+      .then(response => response.json())
+      .then(data => prepareData(data));
   }
 
   const ready = () => {
